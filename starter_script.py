e82e4766fb7f71bae874f92d67c644498e299672
Pap Nóra <jamansardin@gmail.com>
2024-04-05 17:46:08 +0000

    Update

--- starter_script.py
+++ starter_script.py
@@ -4,11 +4,13 @@ import cv2
 
 def save_tracked_video(results, input_video_path, output_video_path):
     cap = cv2.VideoCapture(input_video_path)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-
-    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    ret, frame = cap.read()
+    frame_id = 0 
+    #fps = cap.get(cv2.CAP_PROP_FPS)     
+    width = frame.shape[1]
+    height = frame.shape[0]
+    print(frame.shape)
+    out = cv2.VideoWriter(str(output_video_path), cv2.VideoWriter_fourcc(*'DIVX'), 20.0, (width, height))
 
   #if result is a list and contains elements
     if isinstance(results, list) and len(results) > 0:
@@ -25,7 +27,7 @@ def save_tracked_video(results, input_video_path, output_video_path):
     out.release()
 
 video_path = 'park_people.mp4'
-output_video_path = 'tracked_park_people.mp4'
+output_video_path = 'tracked_park_people.avi'
 
 results = run_tracking(video_path)
 save_tracked_video(results, video_path, output_video_path)
